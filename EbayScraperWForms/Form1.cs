4a0f7a1555f61af5234107ab798cf181966b4d41
ycedillos <yonathan.cedillos@gmail.com>
2017-11-19 23:03:10 -0600

    .Net Class Library to download WebPage

--- EbayScraperWForms/Form1.cs
+++ EbayScraperWForms/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using ScrapySharp.Network;
+using System.Net;
 
 namespace EbayScraperWForms
 {
@@ -13,12 +14,16 @@ namespace EbayScraperWForms
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Helpers.Demo demo = new Helpers.Demo();
+            string response = demo.DownloadPage("https://www.ebay.co.uk/b/Samsung-Galaxy-S8/9355/bn_75787856?LH_ItemCondition=2000&LH_BIN=1");
+
+            /*
             ScrapingBrowser browser = new ScrapingBrowser();
             browser.AllowAutoRedirect = true;
             browser.AllowMetaRedirect = true;
             WebPage PageResult = browser.NavigateToPage(new Uri("https://www.ebay.co.uk/b/Samsung-Galaxy-S8/9355/bn_75787856?LH_ItemCondition=2000&LH_BIN=1"));
             textBox1.Text = PageResult.ToString();
-
+            */
         }
     }
 }
---
+++ Helpers/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class Demo
+    {
+        public string DownloadPage(string url)
+        {
+            string response = string.Empty;
+            using (WebClient client = new WebClient())
+            {
+                response = client.DownloadString(url);
+            }
+            return response;
+        }
+    }
+}
